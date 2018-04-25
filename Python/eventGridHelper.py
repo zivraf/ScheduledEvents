9d17ba90ee45ce346e3fee8263595d78056da3b4
Ziv Rafalovich <zivr@microsoft.com>
2018-04-24 18:00:29 -0700

    Demo scheduled events to EventGrid (serverless)

---
+++ Python/eventGridHelper.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python
+
+import json
+import socket
+import sys, getopt
+import logging
+from enum import Enum
+from datetime import datetime
+import base64
+import hmac
+import hashlib
+import time
+import datetime
+import urllib.request
+import urllib.parse
+import configparser
+from datetime import datetime
+from azure.eventgrid import EventGridClient
+from msrest.authentication import TopicCredentials
+import requests
+
+log_format = " %(asctime)s [%(levelname)s] %(message)s"
+logger = logging.getLogger('ScheduledEvents')
+logging.basicConfig(format=log_format, level=logging.DEBUG)
+this_host=socket.gethostname()
+eventGridSection= 'EVENT-GRID'
+
+
+class EventGridMsgSender:
+     
+    def __init__(self, connectionString=None):
+        if connectionString == None:
+            config = configparser.ConfigParser()
+            config.read('scheduledEventsExtensionConfig.ini')
+            self.topicKey = config.get(eventGridSection,'topic_key')
+            if self.topicKey is None:
+                logger.error ("Failed to load Event Grid key. Make sure config file contains 'topic_key' entry")
+            self.topicEndpoint = config.get(eventGridSection,'topic_endpoint')
+            if self.topicEndpoint is None:
+                logger.error ("Failed to load Event Grid Topic Name. Make sure config file contains 'topic_name' entry")            
+            self.credentials = TopicCredentials(self.topicKey)
+            self.egClient = EventGridClient(self.credentials)
+            self.handleLocalEventsOnly = config.getboolean('DEFAULT','scheduledEvents_handleLocalOnly')
+            if self.handleLocalEventsOnly is None:
+                logger.debug ("Failed to load Event Grid Topic Name. Make sure config file contains 'topic_name' entry")
+                self.handleLocalEventsOnly = False
+
+
+    def send_to_evnt_grid (self, msg):
+        if len(msg['Events']) == 0:
+            logger.debug ("send_to_evnt_grid: No Scheduled Events")
+            return
+        try:            
+            credentials = TopicCredentials(self.topicKey)
+            egClient = EventGridClient(credentials)
+            
+            for event in msg['Events']:
+                eventid=event['EventId']
+                status=event['EventStatus']
+                eventype=event['EventType']
+                restype=event['ResourceType']
+                notbefore=event['NotBefore'].replace(" ","_")
+                isLocal = False
+                for resourceId in event['Resources']:            
+                    if this_host in resourceId or self.handleLocalEventsOnly == False:
+                        self.egClient.publish_events(
+                            self.topicEndpoint,
+                            events=[{
+                                'id' : eventid,
+                                'subject' : "ScheduledEvent:"+eventype+", Host:"+resourceId+", Not Before:"+notbefore,
+                                'data': event,
+                                'event_type': "ScheduledEvent",
+                                'event_time':datetime.now(),
+                                'data_version': "1.0"
+                                }] )
+                        logger.debug ("send_to_evnt_grid: message "+eventid+" was send to EventGrid")
+                    
+        except:
+            logger.error ("send_to_evnt_grid: failed to send ")
---
+++ Python/scheduledEventsHelper.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+
+# Scheduled Event Helper utility 
+
+import json
+import socket
+import sys, getopt
+import logging
+from enum import Enum
+from datetime import datetime
+import base64
+import hmac
+import hashlib
+import time
+import urllib.request
+import urllib.parse
+
+metadata_url="http://169.254.169.254/metadata/scheduledevents?api-version=2017-03-01"
+headers="{Metadata:true}"
+this_host=socket.gethostname()
+log_format = " %(asctime)s [%(levelname)s] %(message)s"
+logger = logging.getLogger('ScheduledEvents')
+logging.basicConfig(format=log_format, level=logging.DEBUG)
+
+class ScheduledEventsHelper:
+
+    def get_scheduled_events(self):
+        logger.debug ("get_scheduled_events was called")
+        req=urllib.request.Request(metadata_url)
+        req.add_header('Metadata','true')        
+        try:
+            resp=urllib.request.urlopen(req)
+            data=json.loads(resp.read().decode('utf8'))
+            '''logger.debug ("scheduled events: \n:"+data)'''
+        except:
+            logger.warn ("get_scheduled_events: No instance metadata . Are you runnign an AZURE VM ? ")
+            return
+
+        return data
+
+    def ack_event(self,evt):
+        for event in evt['Events']:
+            isLocal = False
+            for resourceId in event['Resources']:            
+                if this_host in resourceId:
+                    logger.info ("ack_event was called with eventID "+ event['EventId'])
+                    ack_msg="{\"StartRequests\":[{\"EventId\":\""+event['EventId'] +"\"}]}"
+                    ack_msg=ack_msg.encode()
+                    res=urllib.request.urlopen(metadata_url, data=ack_msg).read()
+                
+    def log_event (self,evt):
+        logger.info ("log_event was called")
+        if len(evt['Events']) == 0:
+            logger.info ("No Scheduled Events")
+            return
+
+        for event in evt['Events']:
+            eventid=event['EventId']
+            status=event['EventStatus']
+            eventype=event['EventType']
+            restype=event['ResourceType']
+            notbefore=event['NotBefore'].replace(" ","_")
+            isLocal = False
+            for resourceId in event['Resources']:            
+                if this_host in resourceId:
+                    isLocal = True 
+
+            logger.info ("EventId: "+ eventid+ " Type: "+ eventype+" Status: "+ status+ " isLocal: "+ str(isLocal))
