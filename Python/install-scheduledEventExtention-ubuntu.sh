a60541a3a823b0ddbe1bbfce00386463f6aa05d7
Ziv Rafalovich <zivr@microsoft.com>
2018-04-26 10:32:32 -0700

    add custom extension

---
+++ Python/install-scheduledEventExtention-ubuntu.sh
@@ -0,0 +1,29 @@
+#Install pip
+pip install proxy.py
+
+#!/bin/bash
+
+# install python3-bottle 
+apt-get -y update
+apt-get install python-pip python-dev build-essential -y
+
+workserver_path=/srv/scheduledEvents
+mkdir $workserver_path
+cp scheduledEventsExtension.py $workserver_path
+cp scheduledEventsExtensionConfig.ini $workserver_path
+cp eventGridHelper.py $workserver_path
+cp scheduledEventsHelper.py $workserver_path
+
+# create a service
+touch /etc/systemd/system/scheduledEvents.service
+printf '[Unit]\nDescription=scheduled events extension\nAfter=rc-local.service\n' >> /etc/systemd/system/scheduledEvents.service
+printf '[Service]\nWorkingDirectory=%s\n' $workserver_path >> /etc/systemd/system/scheduledEvents.service
+printf 'ExecStart=/usr/bin/python3 %s/scheduledEventsExtension.py\n' $workserver_path >> /etc/systemd/system/scheduledEvents.service
+printf 'ExecReload=/bin/kill -HUP $MAINPID\nKillMode=process\nRestart=on-failure\n' >> /etc/systemd/system/scheduledEvents.service
+printf '[Install]\nWantedBy=multi-user.target\nAlias=scheduledEvents.service' >> /etc/systemd/system/scheduledEvents.service
+chmod +x /etc/systemd/system/scheduledEvents.service
+
+# start the  service
+service scheduledEvents start
+
+
