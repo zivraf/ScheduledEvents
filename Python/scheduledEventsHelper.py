285d18f4f7724175536bfc5afa96f5061707c512
zivraf <zivraf@hotmail.com>
2019-01-15 18:45:54 -0800

    Update scheduledEventsHelper.py
    integrate with IMDS for VMSS

--- Python/scheduledEventsHelper.py
+++ Python/scheduledEventsHelper.py
@@ -13,9 +13,6 @@ import hmac
 import hashlib
 import time
 
-# import urllib.request
-# import urllib.parse
-
 try:
     from urllib.parse import urlparse, urlencode
     from urllib.request import urlopen, Request
@@ -25,13 +22,15 @@ except ImportError:
     from urllib import urlencode
     from urllib2 import urlopen, Request, HTTPError
 
-
-metadata_url="http://169.254.169.254/metadata/scheduledevents?api-version=2017-03-01"
+imds_url="http://169.254.169.254/metadata/instance?api-version=2017-08-01"
+imds_headers="{Metadata:true}"
+metadata_url="http://169.254.169.254/metadata/scheduledevents?api-version=2017-08-01"
 headers="{Metadata:true}"
 this_host=socket.gethostname()
 log_format = " %(asctime)s [%(levelname)s] %(message)s"
 logger = logging.getLogger('ScheduledEvents')
 logging.basicConfig(format=log_format, level=logging.DEBUG)
+imds_data = "none"
 
 class ScheduledEventsHelper:
 
@@ -47,19 +46,52 @@ class ScheduledEventsHelper:
             resp=urlopen(req)
             data=json.loads(resp.read().decode('utf8'))            
         except:
-            logger.warn ("get_scheduled_events: No instance metadata . Are you runnign an AZURE VM ? ")
+            logger.warn ("get_scheduled_events: No instance metadata . Are you running an AZURE VM ? ")
             return        
         return data
 
+    def get_imds_local_host(self):
+        logger.warn ("get_imds_local_host was called")
+        # Call IMDS to identify the host name
+        try: 
+            imds_req=Request(imds_url)
+            imds_req.add_header('Metadata','true')                    
+        except Exception:
+            logger.debug ("get_imds_local_host: failed to set a request ")
+                    
+        try:
+            imds_resp=urlopen(imds_req)
+            imds_data=json.loads(imds_resp.read().decode('utf8'))            
+        except:
+            logger.warn ("get_imds_local_host: No instance metadata . Are you running an AZURE VM ? ")
+        
+        logger.debug ("get_imds_local_host: IMDS hostname is "+imds_data["compute"]["name"])
+        return imds_data["compute"]["name"]
+
+    def is_local_event (self,evt,localHostName):
+        logger.warn ("is_local_event was called")
+        
+        isLocal = False
+        for event in evt['Events']:
+            for resourceId in event['Resources']:            
+                logger.debug ("is_local_event: compare with resource in event: "+resourceId)
+
+                if localHostName in resourceId:
+                    logger.info ("ack_event was called on local host with eventID "+ event['EventId'])
+                    isLocal = True
+
+        return isLocal
+        
     def ack_event(self,evt):
         for event in evt['Events']:
-            isLocal = False
             for resourceId in event['Resources']:            
-                if this_host in resourceId:
-                    logger.info ("ack_event was called with eventID "+ event['EventId'])
+                if imds_data["compute"]["name"] in resourceId:
+                    logger.info ("ack_event was called on local host with eventID "+ event['EventId'])
                     ack_msg="{\"StartRequests\":[{\"EventId\":\""+event['EventId'] +"\"}]}"
                     ack_msg=ack_msg.encode()
                     res=urlopen(metadata_url, data=ack_msg).read()
+                    return True
+        return False
                 
     def log_event (self,evt):
         logger.info ("log_event was called")
@@ -73,9 +105,6 @@ class ScheduledEventsHelper:
             eventype=event['EventType']
             restype=event['ResourceType']
             notbefore=event['NotBefore'].replace(" ","_")
-            isLocal = False
-            for resourceId in event['Resources']:            
-                if this_host in resourceId:
-                    isLocal = True 
+            isLocal = False            
 
-            logger.info ("EventId: "+ eventid+ " Type: "+ eventype+" Status: "+ status+ " isLocal: "+ str(isLocal))
+            logger.info ("EventId: "+ eventid+ " Type: "+ eventype+" Status: "+ status)
